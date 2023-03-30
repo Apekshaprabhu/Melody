443359e89c10c03ec292c9bfdda0a2a401bc1ea6
Aishwarya Shetty <67544316+aishwarya0001@users.noreply.github.com>
2023-03-30 15:21:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onSpotifyWebPlaybackSDKReady = () => {
-  const token = "[My Spotify Web API access token]";
-  const player = new Spotify.Player({
-    name: "Web Playback SDK Quick Start Player",
-    getOAuthToken: (cb) => {
-      cb(token);
-    },
-  });
-};
